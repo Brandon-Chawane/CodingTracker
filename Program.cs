7505d4be7ee35b56b9ba9e2afedc4d46fa9c04ff
Brandon Chawane <brandon.chawane@gmail.com>
2025-02-01 09:39:04 +0200

    added a number

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#!");
+            int number = 6;
+            Console.WriteLine("Hello, C#!" + number);
         }
     }
 }
